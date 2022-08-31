7d3efdba375db4821893951cbd05a89e8615167c
jiangqb <jiangqb@citycloud.com.cn>
2022-08-31 23:29:23 +0800

    feat(add devserver): add devserver

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,22 @@
   <div id="app">
     <h1>彪哥教育</h1>
     <!-- 根路由出口 -->
+    <p class="text">Hello world</p>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
 <style lang="scss" scoped>
+// @import '~@/styles/variables.scss';
+.text {
+  color: $warning-color;
+}
 </style>
